5fe20a8573d644bb91ec813340877e7d7067a7d3
andre.vinicius.moura@outlook.com <andre.vinicius.moura@outlook.com>
2020-04-29 21:04:33 -0300

    feat: Inclusão de notificações.

--- src/PaymentContext/PaymentContext.Domain/ValueObjects/Name.cs
+++ src/PaymentContext/PaymentContext.Domain/ValueObjects/Name.cs
@@ -21,6 +21,9 @@ namespace PaymentContext.Domain.ValueObjects
         {
             FirstName = firstName;
             LastName = lastName;
+
+            if (string.IsNullOrEmpty(FirstName))
+                AddNotification("Name.FirstName", "Nome inválido");
         }
 
         #endregion
