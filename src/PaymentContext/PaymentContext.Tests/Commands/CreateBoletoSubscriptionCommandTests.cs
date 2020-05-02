8461630e08ae2304766f975e39e39a9e9fab03af
andre.vinicius.moura@outlook.com <andre.vinicius.moura@outlook.com>
2020-05-02 18:12:12 -0300

    feat: Testando commands

---
+++ src/PaymentContext/PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContext.Tests.Commands
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        //Red, Green, Refactor
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "";
+
+            command.Validate();
+
+            Assert.AreEqual(false, command.Valid);
+        }
+    }
+}
